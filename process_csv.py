b2ae6eb55739d324533824174768c3fca2e81601
jorgemdg95 <122839483+jorgemdg95@users.noreply.github.com>
2024-08-05 11:03:52 -0600

    Update process_csv.py

--- process_csv.py
+++ process_csv.py
@@ -1,9 +1,1 @@
-import pandas as pd
-
-# Read the CSV file
-df = pd.read_csv('test.csv')
-
-# Multiply the 'test1' column by 2
-df['test1'] = df['test1'] * 2
-
-print(df)
+print("Me gusta el helado")
